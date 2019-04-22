43ff17c7181758304c7451131070b6fed4bb1519
acoshift <acoshift@gmail.com>
2019-04-23 00:04:06 +0700

    add log

--- README.md
+++ README.md
@@ -1,5 +1,81 @@
 # tirev
 
-Tiny Reverse Proxy
+Tiny Sidecar Reverse Proxy Powered by [Parapet](https://github.com/moonrhythm/parapet)
 
 > gcr.io/moonrhythm-containers/tirev
+
+### Example
+
+#### Kubernetes
+
+```yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  labels:
+    app: echoserver
+  name: echoserver
+spec:
+  replicas: 2
+  selector:
+    matchLabels:
+      app: echoserver
+  template:
+    metadata:
+      annotations:
+        prometheus.io/port: "9187"
+        prometheus.io/scrape: "true"
+      labels:
+        app: echoserver
+      name: echoserver
+    spec:
+      containers:
+      - name: echoserver
+        image: gcr.io/google-containers/echoserver:1.10
+        ports:
+        - containerPort: 8080
+      - name: tirev
+        image: gcr.io/moonrhythm-containers/tirev
+        env:
+        - name: PORT
+          value: "80"
+        - name: UPSTREAM_ADDR
+          value: 127.0.0.1:8080
+        - name: RATELIMIT_S
+          value: "30"
+        - name: BODY_LIMITREQUEST
+          value: "15728640"
+        ports:
+        - containerPort: 80
+        livenessProbe:
+          httpGet:
+            path: /healthz
+            port: 80
+            scheme: HTTP
+          successThreshold: 1
+          failureThreshold: 2
+          timeoutSeconds: 5
+        readinessProbe:
+          httpGet:
+            path: /healthz?ready=1
+            port: 80
+            scheme: HTTP
+          successThreshold: 1
+          failureThreshold: 2
+          timeoutSeconds: 5
+---
+apiVersion: v1
+kind: Service
+metadata:
+  labels:
+    app: echoserver
+  name: echoserver
+spec:
+  type: ClusterIP
+  selector:
+    app: echoserver
+  ports:
+  - name: http
+    port: 80
+    targetPort: 80
+```
--- main.go
+++ main.go
@@ -46,99 +46,132 @@ var (
 	redirectWWW       = config.String("redirect_www") // "", "www", "non"
 	upstreamAddr      = config.String("upstream_addr")
 	upstreamProto     = config.String("upstream_proto") // http, h2c, https, unix
+	gcpHLB            = config.IntDefault("gcp_hlb", -1)
 )
 
 func main() {
+	fmt.Println("tirev")
+	fmt.Println()
+
 	var s *parapet.Server
 	if front {
 		s = parapet.NewFrontend()
+		fmt.Println("Parapet Frontend Server")
 	} else {
 		s = parapet.New()
+		fmt.Println("Parapet Server")
 	}
 
 	if !noHealthz {
-
 		h := host.NewCIDR("0.0.0.0/0")
 		l := location.Exact(healthzPath)
 		l.Use(healthz.New())
 		h.Use(l)
 
 		s.Use(h)
+		fmt.Println("Registered healthz at", healthzPath)
 	}
 	if !noProm {
 		s.Use(prom.Requests())
 	}
-	s.Use(gcp.HLBImmediateIP(0))
+
+	if gcpHLB >= 0 {
+		s.Use(gcp.HLBImmediateIP(gcpHLB))
+		fmt.Println("Registered GCP HLB Immediate IP")
+	}
 
 	if !noLog {
 		s.Use(logger.Stdout())
+		fmt.Println("Registered Logger")
 	}
 
 	if !noReqID {
 		s.Use(requestid.New())
+		fmt.Println("Registered Request ID")
 	}
 
 	if ratelimitS > 0 {
 		s.Use(ratelimit.FixedWindowPerSecond(ratelimitS))
+		fmt.Println("Registered Ratelimiter (second):", ratelimitS)
 	}
 	if ratelimitM > 0 {
 		s.Use(ratelimit.FixedWindowPerMinute(ratelimitM))
+		fmt.Println("Registered Ratelimiter (minute):", ratelimitM)
 	}
 	if ratelimitH > 0 {
 		s.Use(ratelimit.FixedWindowPerMinute(ratelimitH))
+		fmt.Println("Registered Ratelimiter (hour):", ratelimitH)
 	}
 
 	if bodyLimitRequest > 0 {
 		s.Use(body.LimitRequest(bodyLimitRequest))
+		fmt.Println("Registered Request Body Limiter:", bodyLimitRequest)
 	}
 	if bodyBufferRequest {
 		s.Use(body.BufferRequest())
+		fmt.Println("Registered Request Body Bufferer")
 	}
 
 	if !noGzip {
 		s.Use(compress.Gzip())
+		fmt.Println("Registered Gzip Compressor")
 	}
 	if !noBr {
 		s.Use(compress.Br())
+		fmt.Println("Registered Br Compressor")
 	}
 
 	if redirectHTTPS {
 		s.Use(redirect.HTTPS())
+		fmt.Println("Registered HTTPS Redirector")
 	}
 
 	if hstsMode == "preload" {
 		s.Use(hsts.Preload())
+		fmt.Println("Registered HSTS Preload")
 	} else if hstsMode != "" {
 		s.Use(hsts.Default())
+		fmt.Println("Registered HSTS")
 	}
 
 	if redirectWWW == "www" {
 		s.Use(redirect.WWW())
+		fmt.Println("Registered WWW Redirector")
 	} else if redirectWWW == "non" {
 		s.Use(redirect.NonWWW())
+		fmt.Println("Registered Non-WWW Redirector")
 	}
 
 	var tr http.RoundTripper
 	switch upstreamProto {
 	default:
 		tr = &transport.HTTP{}
+		fmt.Println("Using HTTP Transport")
 	case "https":
 		tr = &transport.HTTPS{}
+		fmt.Println("Using HTTPS Transport")
 	case "h2c":
 		tr = &transport.H2C{}
+		fmt.Println("Using H2C Transport")
 	case "unix":
 		tr = &transport.Unix{}
+		fmt.Println("Using Unix Transport")
 	}
 
 	s.Use(upstream.SingleHost(upstreamAddr, tr))
+	fmt.Println("Upstream", upstreamAddr)
 
 	if !noProm {
 		prom.Connections(s)
 		prom.Networks(s)
 		go prom.Start(fmt.Sprintf(":%d", promPort))
+		fmt.Println("Starting prometheus on port", promPort)
 	}
 
 	s.Addr = fmt.Sprintf(":%d", port)
+	fmt.Println("Starting parapet on port", port)
+	fmt.Println()
+
 	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
