3dfb604f331f7cc0bbb0345f046ba0c748f71d51
acoshift <acoshift@gmail.com>
2019-04-23 23:55:44 +0700

    add header add, set, del

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/acoshift/configfile"
 	"github.com/moonrhythm/parapet"
 	"github.com/moonrhythm/parapet/pkg/body"
 	"github.com/moonrhythm/parapet/pkg/compress"
 	"github.com/moonrhythm/parapet/pkg/gcp"
+	"github.com/moonrhythm/parapet/pkg/headers"
 	"github.com/moonrhythm/parapet/pkg/healthz"
 	"github.com/moonrhythm/parapet/pkg/host"
 	"github.com/moonrhythm/parapet/pkg/hsts"
@@ -36,6 +38,12 @@ var (
 	noBr              = config.Bool("no_br")
 	noLog             = config.Bool("no_log")
 	noReqID           = config.Bool("no_reqid")
+	reqHeaderSet      = parseHeaders(config.String("reqheader_set"))
+	reqHeaderAdd      = parseHeaders(config.String("reqheader_add"))
+	reqHeaderDel      = parseHeaders(config.String("reqheader_del"))
+	respHeaderSet     = parseHeaders(config.String("respheader_set"))
+	respHeaderAdd     = parseHeaders(config.String("respheader_add"))
+	respHeaderDel     = parseHeaders(config.String("respheader_del"))
 	ratelimitS        = config.Int("ratelimit_s")
 	ratelimitM        = config.Int("ratelimit_m")
 	ratelimitH        = config.Int("ratelimit_h")
@@ -46,6 +54,9 @@ var (
 	redirectWWW       = config.String("redirect_www") // "", "www", "non"
 	upstreamAddr      = config.String("upstream_addr")
 	upstreamProto     = config.String("upstream_proto") // http, h2c, https, unix
+	upstreamHeaderSet = parseHeaders(config.String("upstream_header_set"))
+	upstreamHeaderAdd = parseHeaders(config.String("upstream_header_add"))
+	upstreamHeaderDel = parseHeaders(config.String("upstream_header_del"))
 	gcpHLB            = config.IntDefault("gcp_hlb", -1)
 )
 
@@ -71,6 +82,32 @@ func main() {
 		s.Use(h)
 		fmt.Println("Registered healthz at", healthzPath)
 	}
+
+	if len(reqHeaderSet) > 0 {
+		s.Use(headers.SetRequest(reqHeaderSet...))
+		fmt.Println("Registered Request Header Setter")
+	}
+	if len(reqHeaderAdd) > 0 {
+		s.Use(headers.AddRequest(reqHeaderAdd...))
+		fmt.Println("Registered Request Header Adder")
+	}
+	if len(reqHeaderDel) > 0 {
+		s.Use(headers.DeleteRequest(reqHeaderDel...))
+		fmt.Println("Registered Request Header Deleter")
+	}
+	if len(respHeaderSet) > 0 {
+		s.Use(headers.SetResponse(respHeaderSet...))
+		fmt.Println("Registered Response Header Setter")
+	}
+	if len(respHeaderAdd) > 0 {
+		s.Use(headers.AddResponse(respHeaderAdd...))
+		fmt.Println("Registered Response Header Adder")
+	}
+	if len(respHeaderDel) > 0 {
+		s.Use(headers.DeleteResponse(respHeaderDel...))
+		fmt.Println("Registered Response Header Deleter")
+	}
+
 	if !noProm {
 		s.Use(prom.Requests())
 	}
@@ -142,6 +179,19 @@ func main() {
 		fmt.Println("Registered Non-WWW Redirector")
 	}
 
+	if len(upstreamHeaderSet) > 0 {
+		s.Use(headers.SetRequest(upstreamHeaderSet...))
+		fmt.Println("Registered Upstream Header Setter")
+	}
+	if len(upstreamHeaderAdd) > 0 {
+		s.Use(headers.AddRequest(upstreamHeaderAdd...))
+		fmt.Println("Registered Upstream Header Adder")
+	}
+	if len(upstreamHeaderDel) > 0 {
+		s.Use(headers.DeleteRequest(upstreamHeaderDel...))
+		fmt.Println("Registered Upstream Header Deleter")
+	}
+
 	var tr http.RoundTripper
 	switch upstreamProto {
 	default:
@@ -176,4 +226,24 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func parseHeaders(s string) []string {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return nil
+	}
+
+	ss := strings.Split(s, ",")
+
+	var rs []string
+	for _, x := range ss {
+		ps := strings.Split(x, ":")
+		if len(ps) != 2 {
+			continue
+		}
+		rs = append(rs, strings.TrimSpace(ps[0]), strings.TrimSpace(ps[1]))
+	}
+
+	return rs
 }
